86867a9296f0acdc8ef84cfc25b796a5bce76593
longwei66 <longwei66@gmail.com>
2015-07-20 23:37:48 +0800

    adding documentation in the app

--- weather-shiny-app/ui.R
+++ weather-shiny-app/ui.R
@@ -11,7 +11,7 @@ weather <- load_data("./data/severe-weather-compact-db.csv")
 shinyUI(pageWithSidebar(
         headerPanel("Travel in USA ~ severe weather risk assesment"),
         sidebarPanel(
-                selectInput('destinationState', 'Where are you going ?', state.name),
+                selectInput('destinationState', 'Where are you going to ?', state.name),
                 sliderInput('monthView', 'Which month are you travelling ?', min = 1, max = 12, value = 3),
                 selectInput('variableView', 'What is your concern?', c("Death or Injury" = "health_impact",
                                                                        "Property and Crop Damages" = "DMG",
@@ -45,9 +45,13 @@ shinyUI(pageWithSidebar(
                                  dataTableOutput('bestState')
                         ),
                         tabPanel("Documentation",
-                                 p('data sources'),
-                                 p('scrip sources'),
-                                 p('license')
+                                p('The purpose of this app is to inform the user about the biggest potential server weather risk she/he may encounter while travelling in a specific state for a specific month. Then the app will recommend the best month to travel to this location as well as where in USA would be the safest during the same period.'),
+                                p('In addition, a map of USA illustrates the distribution risk of similar severe weather events during the same period.'),
+                                 hr(),
+                                 p('To use the app, select your destination of travel (State name), and the month of travel.'),
+                                p('Per default, the app use a composite risk index to compare the severe weather events. Nevertheless, other choice are possible, such as health impact, measuring the total fatalitites and injuries over the period or the damages, measuring the total damages in USD over the period, both for property and crop'),
+                                hr(),
+                                 p('The data used by this app is a cleaned and tidy data set of severe weather events in the US since 1950 evaluated from both financial and health impact cumed for each month of the year, each state and event type. Initial data is from the NOAA Storm Database (see http://rpubs.com/longwei66/87696).')
                         )
                 )
         )
