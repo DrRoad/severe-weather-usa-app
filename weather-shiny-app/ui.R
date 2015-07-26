1eee0b19964d2aec524f2843f0de79edbdcd6903
longwei66 <longwei66@gmail.com>
2015-07-26 15:22:10 +0800

    Modify the documentation to add links to shiny presentations and git code

--- weather-shiny-app/ui.R
+++ weather-shiny-app/ui.R
@@ -45,13 +45,24 @@ shinyUI(pageWithSidebar(
                                  dataTableOutput('bestState')
                         ),
                         tabPanel("Documentation",
+                                 h3('Objective'),
                                 p('The purpose of this app is to inform the user about the biggest potential server weather risk she/he may encounter while travelling in a specific state for a specific month. Then the app will recommend the best month to travel to this location (for which the risk is the lowest) as well as where in USA would be the safest during the same period.'),
                                 p('In addition, a map of USA illustrates the distribution risk of similar severe weather events during the same period.'),
                                  hr(),
+                                h3('How to use the app ?'),
                                  p('To use the app, select your destination of travel (State name), and the month of travel.'),
                                 p('Per default, the app use a composite risk index to compare the severe weather events. Nevertheless, other choice are possible, such as health impact, measuring the total fatalitites and injuries over the period or the damages, measuring the total damages in USD over the period, both for property and crop'),
                                 hr(),
-                                 p('The data used by this app is a cleaned and tidy data set of severe weather events in the US since 1950 evaluated from both financial and health impact cumed for each month of the year, each state and event type. Initial data is from the NOAA Storm Database (see http://rpubs.com/longwei66/87696).')
+                                 p('The data used by this app is a cleaned and tidy data set of severe weather events in the US since 1950 evaluated from both financial and health impact cumed for each month of the year, each state and event type. Initial data is from the NOAA Storm Database:'),
+                                a('http://rpubs.com/longwei66/87696', href = 'http://rpubs.com/longwei66/87696'), 
+                                hr(),
+                                h3('More documentation :'),
+                                p('A presentation of this app is available on Rpubs : '),
+                                a('http://rpubs.com/longwei66/swa', href = 'http://rpubs.com/longwei66/swa'),
+                                hr(),
+                                h3('Source code'),
+                                p('The source code of this app is available on github :'),
+                                a('https://github.com/longwei66/severe-weather-usa-app/tree/master/weather-shiny-app', href = "https://github.com/longwei66/severe-weather-usa-app/tree/master/weather-shiny-app")
                         )
                 )
         )
